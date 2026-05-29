3d343c877fbff559310d7ad99e37d2598dc29db6
Ehlakya <ehlakyas@gmail.com>
2026-05-29 16:47:22 +0530

    ensure initDB runs on startup to sync schema changes

--- server.js
+++ server.js
@@ -128,6 +128,9 @@ app.get("/", (req, res) => {
   res.send("Backend Running Successfully 🚀");
 });
 
+const initDB = require("./config/initDb");
+initDB();
+
 server.listen(PORT, () => {
   console.log(`
 🚀 Server running on http://localhost:${PORT}
